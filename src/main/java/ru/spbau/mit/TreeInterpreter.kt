7a2b3c94b73706189132c09b61217e1d6ad7956b
Andrey Tonkikh <xosmig@gmail.com>
2017-11-10 21:50:20 +0300

    Add `TreeInterpreter`

---
+++ src/main/java/ru/spbau/mit/TreeInterpreter.kt
@@ -0,0 +1,193 @@
+package ru.spbau.mit
+
+import java.util.*
+import kotlin.collections.HashMap
+
+sealed class Statement {
+    abstract fun perform(ctx: Context)
+}
+
+class StackFrame {
+    private val vars = HashMap<String, Int>()
+    private val funs = HashMap<String, Function>()
+
+    fun getVar(name: String): Int? = vars[name]
+
+    fun addVar(name: String, value: Int) {
+        vars[name] = value
+    }
+
+    fun getFun(name: String): Function? = funs[name]
+
+    fun addFun(name: String, func: Function) {
+        funs[name] = func
+    }
+}
+
+class Context {
+    private val callStack = LinkedList<StackFrame>()
+    private val top = callStack.first
+
+    var maxStackSize = 1000
+
+    fun getVar(name: String): Int {
+        for (frame in callStack) {
+            val value = frame.getVar(name)
+            if (value != null) {
+                return value
+            }
+        }
+        throw UnknownIdentifierException(name, this)
+    }
+
+    fun addVar(name: String, value: Int) {
+        top.addVar(name, value)
+    }
+
+    fun assignVar(name: String, value: Int) {
+        for (frame in callStack) {
+            val oldValue = frame.getVar(name)
+            if (oldValue != null) {
+                frame.addVar(name, value)
+            }
+        }
+        throw UnknownIdentifierException(name, this)
+    }
+
+    fun getFun(name: String): Function {
+        for (frame in callStack) {
+            val func = frame.getFun(name)
+            if (func != null) {
+                return func
+            }
+        }
+        throw UnknownIdentifierException(name, this)
+    }
+
+    fun addFun(name: String, func: Function) {
+        top.addFun(name, func)
+        if (callStack.size > maxStackSize) {
+            throw TooDeepRecursionException(callStack.size)
+        }
+    }
+
+    fun addStackFrame() {
+        callStack.addFirst(StackFrame())
+    }
+
+    fun popStackFrame() {
+        callStack.removeFirst()
+    }
+}
+
+data class File(val block: Block) {
+    fun perform() {
+        block.perform(Context())
+    }
+}
+
+data class Function(val name: String, val argsNames: List<String>, val block: Block): Statement() {
+    override fun perform(ctx: Context) = ctx.addFun(name, this)
+}
+
+data class Variable(val name: String, val expr: Expression): Statement() {
+    override fun perform(ctx: Context) {
+        ctx.addVar(name, expr.evaluate(ctx))
+    }
+}
+
+data class While(val cond: Expression, val block: Block): Statement() {
+    override fun perform(ctx: Context) {
+        while (cond.evaluate(ctx) != 0) {
+            block.perform(ctx)
+        }
+    }
+}
+
+sealed class Expression: Statement() {
+    abstract fun evaluate(ctx: Context): Int
+
+    override fun perform(ctx: Context) {
+        evaluate(ctx)
+    }
+}
+
+data class FunctionCall(val name: String, val argsExprs: List<Expression>): Expression() {
+    override fun evaluate(ctx: Context): Int {
+        ctx.addStackFrame()
+        try {
+            val func = ctx.getFun(name)
+            if (func.argsNames.size != argsExprs.size) {
+                throw WrongNumberOfArgsException(name, func.argsNames.size, argsExprs.size, ctx)
+            }
+            for ((name, expr) in func.argsNames.zip(argsExprs)) {
+                ctx.addVar(name, expr.evaluate(ctx))
+            }
+            func.block.perform(ctx)
+        } catch (ret: ReturnException) {
+            return ret.value
+        } finally {
+            ctx.popStackFrame()
+        }
+        return 0
+    }
+}
+
+
+data class Plus(val lhs: Expression, val rhs: Expression): Expression() {
+    override fun evaluate(ctx: Context): Int = lhs.evaluate(ctx) + rhs.evaluate(ctx)
+}
+
+//class Minus(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+//class Times(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+//class Div(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+//class Modulo(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+//class Greater(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+//class Less(lhs: Expression, rhs: Expression): BinaryExpression(lhs, rhs)
+// TODO: >=, <=, ==, !=, ||, &&
+
+data class Regerence(val name: String): Expression() {
+    override fun evaluate(ctx: Context): Int = ctx.getVar(name)
+}
+
+data class Literal(val value: Int): Expression() {
+    override fun evaluate(ctx: Context): Int = value
+}
+
+data class Return(val expr: Expression): Statement() {
+    override fun perform(ctx: Context) = throw ReturnException(expr.evaluate(ctx))
+}
+
+data class Assignment(val name: String, val expr: Expression): Statement() {
+    override fun perform(ctx: Context) = ctx.assignVar(name, expr.evaluate(ctx))
+}
+
+data class Block(val statements: List<Statement>): Statement() {
+    override fun perform(ctx: Context) {
+        ctx.addStackFrame()
+        try {
+            for (st in statements) {
+                st.perform(ctx)
+            }
+        } finally {
+            ctx.popStackFrame()
+        }
+    }
+}
+
+data class PrintlnCall(val args: List<Expression>): Statement() {
+    override fun perform(ctx: Context) {
+        for (arg in args) {
+            print(arg)
+            print(' ')
+        }
+        println()
+    }
+}
+
+data class ReturnException(val value: Int): Exception()
+
+data class WrongNumberOfArgsException(val name: String, val expected: Int, val action: Int,
+                                      val ctx: Context): Exception()
+data class UnknownIdentifierException(val name: String, val ctx: Context): Exception()
+data class TooDeepRecursionException(val depth: Int): Exception()
